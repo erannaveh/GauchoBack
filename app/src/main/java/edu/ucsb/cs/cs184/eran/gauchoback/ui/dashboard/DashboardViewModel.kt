d8458ff1502ae40e69c90df829493d55ac0875af
Eran Naveh <eran@umail.ucsb.edu>
2020-11-30 20:59:32 -0800

    initial commit

---
+++ app/src/main/java/edu/ucsb/cs/cs184/eran/gauchoback/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package edu.ucsb.cs.cs184.eran.gauchoback.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
